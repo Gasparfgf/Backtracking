948d7905ede97f7677a594cc0660686940a80c31
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2025-05-09 05:47:56 +0200

    feat : create skeleton parallel by direction file

---
+++ src/parallel_direction/ParallelDirectionSolver.hpp
@@ -0,0 +1,19 @@
+#ifndef PARALLEL_DIRECTION_SOLVER_H
+#define PARALLEL_DIRECTION_SOLVER_H
+
+#include "../common/MazeCommon.hpp"
+#include <functional>
+
+class ParallelDirectionSolver : public MazeSolver {
+public:
+    bool solveMaze() override;
+
+private:
+    bool solveMazeByDirectionParallel(Position start);
+    
+    bool solveDirection(Position pos, bool hasB, bool hasC, bool hasE, 
+                       std::vector<Position>& threadPath, 
+                       std::vector<Level>& threadLevels);
+};
+
+#endif // PARALLEL_DIRECTION_SOLVER_H
